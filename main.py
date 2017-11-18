a284799a6c658bf85114b619e47113dd0e8589a9
rlam83 <33775632+rlam83@users.noreply.github.com>
2017-11-18 02:51:41 -0500

    Added new routes

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    return 'Homepage!'
+
+
+@app.route('/hello')
+def hello():
+    name = request.args.get('name')
+    try:
+        age = int(request.args.get('age'))
+    except TypeError:
+        age = 0
+    age_msg = None
+
+    if age < 30:
+        age_msg = "You're quite young!"
+    else:
+        age_msg = "You're getting old!"
+
+    return 'Hello {name}! {age_str}'.format(name=name, age_str=age_msg)
+
 
 if __name__ == "__main__":
     app.run()
